aa79603bb29c196d81b0eac9bbebcd1fff9f76e2
Debbie <nuna@debbie.vm.com>
2024-01-26 09:00:45 -0500

    something is fcked with mariadb, root? passwd change is commented, try to delete volumes and re run the scrupt. also the setup script is commented out

--- srcs/mariadb/conf/setup.sh
+++ srcs/mariadb/conf/setup.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 # Start the MySQL service to be able to run the script
 service mysql start
 
 
 touch myscript.sql
-
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;
-
+echo '2'
+echo "
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
-
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 FLUSH PRIVILEGES;" \
 	> myscript.sql
 
+
+echo '3'
+cat myscript.sql
 # Execute the SQL script
+# mysql --user=root --password=$DB_ROOT_PASSWORD < myscript.sql
 mysql < myscript.sql
 
-# Stop the MySQL service
-service mysql stop
 
+echo '4'
+# Stop the MySQL service
+# service mysql stop
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+echo '5'
 # Start the mysqld deamon in the foreground to keep the container running
 mysqld
